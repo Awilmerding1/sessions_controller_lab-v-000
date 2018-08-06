934ec40157325d928c6eabd25a54603ff4426583
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-06 18:43:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end 
   
   def create 
+    if params[:name] == nil || ""
     session[:name] = params[:name]
       redirect_to '/'
-    else 
-    redirect_to '/sessions/new'
+    
   end
   end 
   
